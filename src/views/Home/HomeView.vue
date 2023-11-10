be1ff531d8461b4fc945f458e2daf1cfe4093abb
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-10 15:43:38 +0900

    feat: About Container #7
    - Home, Board, About

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div id="map">지도입니다.</div>
+    <TheSide :position-left="0"></TheSide>
+    <!-- <TheSide :position-left="340"></TheSide> -->
+  </main>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import TheSide from "../../components/TheSideBar/TheSideBar.vue";
+
+const sideBarViewStates = reactive({ cardsSideBarView: false, reviewsSideBarView: false });
+</script>
+
+<style lang="scss">
+@import "./HomeView.scss";
+</style>
