0f119457e88e5ccdfd80e17de8fbf4d32208f87a
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-10 15:05:39 +0900

    build: initial setting
    - init package
    - create palette

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from "vue-router";
+import Header from "../src/components/TheHeader/TheHeader.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/palette.js
@@ -0,0 +1,8 @@
+export const PALETTE = {
+  MAIN_BLUE: "var(--main-blue)",
+  MAIN_BLACK: "var(--main-black)",
+  SUB_BLACK: "var(--sub-black)",
+  LIGHT_BLACK: "var(--light-black)",
+  MAIN_GRAY: "var(--main-gray)",
+  SUB_GRAY: "var(--sub-gray)"
+};
