ed21efe51f4200f7c5777f7ae02304b6f7f02265
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-13 16:28:57 +0900

    feat: Modify TheSideBar #5
    - Modify import dir
    - Add param in handlers.reviewVisible function

--- src/components/TheSideBar/TheSideBar.vue
+++ src/components/TheSideBar/TheSideBar.vue
@@ -1,6 +1,7 @@
 <template>
-  <aside :class="`side-bar-wrapper ${sideBarState.className}`" :style="{ left: `${positionLeft}px` }">
-    <div class="side-bar-opener-wrapper">
+  <aside :class="`card-side-bar-wrapper ${cardStates.className}`" :style="{ left: `${positionLeft}px` }">
+    <div class="card-side-bar-container">
+      <TheReviewSideBar :positionLeft="340" @viewHandler="handlers.reviewVisible"></TheReviewSideBar>
       <Button
         class="opener"
         type="button"
@@ -8,22 +9,29 @@
         height="48px"
         :color="PALETTE.MAIN_BLACK"
         backgroundColor="white"
-        :eventHandler="handleSideBarVisible"
+        :eventHandler="handlers.cardVisible"
         :sortCenter="true"
       >
-        <img src="../../assets/imgs/arrow-right.svg" v-if="sideBarState.visible" width="12" height="12" />
+        <img src="../../assets/imgs/arrow-right.svg" v-if="!cardStates.visible" width="12" height="12" />
         <img src="../../assets/imgs/arrow-left.svg" v-else width="12" height="12" />
       </Button>
       <ul class="cards-container">
-        <TheReviewCard v-for="(item, index) in DUMMY_ITEMS" :key="index">
+        <TheReviewCard
+          v-for="(item, index) in DUMMY_ITEMS"
+          :key="index"
+          :contentId="index + 1"
+          :reviewSideBarHandler="handlers.reviewVisible"
+        >
           <p class="side-card-title">{{ item.title }}</p>
           <p class="side-card-address-1">{{ item.addr1 }} {{ item.addr2 }}</p>
+          <!-- address -->
           <p class="side-card-address-2"></p>
           <p class="side-card-star-point">
             <img src="../../assets/imgs/star.svg" width="14" height="14" />{{ item.starPoint }}
           </p>
           <p class="side-card-review-count">리뷰({{ item.tel.length }})</p>
           <img class="side-card-img" :src="item.firstimage" />
+          <!-- imageUrl -->
         </TheReviewCard>
       </ul>
     </div>
@@ -34,33 +42,35 @@
 import { reactive, ref, toRefs } from "vue";
 import { PALETTE } from "../../palette";
 import Button from "../BaseButton/BaseButton.vue";
-import TheReviewCard from "./Subs/TheReviewCard.vue";
+import TheReviewCard from "./Subs/TheReviewCard/TheReviewCard.vue";
 import { DUMMY_ITEMS } from "../../dummy";
+import TheReviewSideBar from "./Subs/TheReviewSideBar/TheReviewSideBar.vue";
 
-const props = defineProps({ positionLeft: { type: Number, required: true } });
-const { positionLeft } = toRefs(props);
-console.log(`positionLeft.value :`, positionLeft.value);
+const props = defineProps({
+  positionLeft: { type: Number, required: true }
+});
 
-const sideBarState = reactive({ visible: true, className: "show" });
-const items = ref(
-  Array.from({ length: 40 }).map((_) => ({
-    title: "후라토식당 경복궁 본점",
-    starPoint: 4.5,
-    reviewCount: 120,
-    add1: "서울 종로구 사직로8길 34",
-    add2: "경희궁의아침 3단지 사발 142호",
-    content: "CARDS",
-    img: "src/assets/imgs/unknowen-user.svg"
-  }))
-);
+const { view, positionLeft } = toRefs(props);
 
-const handleSideBarVisible = () => {
-  sideBarState.visible = !sideBarState.visible;
-  console.log(`sideBarState.visible :`, sideBarState.visible);
-  if (sideBarState.visible) {
-    sideBarState.className = "show";
-  } else {
-    sideBarState.className = "hidden";
+const cardStates = reactive({ className: "show", visible: true });
+const reviewStates = reactive({ className: "hidden", visible: false });
+
+const handlers = {
+  cardVisible() {
+    cardStates.visible = !cardStates.visible;
+    if (cardStates.visible) {
+      cardStates.className = "show";
+    } else {
+      cardStates.className = "hidden";
+    }
+  },
+  reviewVisible(isVisible) {
+    reviewStates.visible = isVisible;
+    if (reviewStates.visible) {
+      reviewStates.className = "show";
+    } else {
+      reviewStates.className = "hidden";
+    }
   }
 };
 </script>
