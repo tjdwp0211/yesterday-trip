085b052717978009a4daa95bcfd1f875fc0e52e2
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-13 16:24:28 +0900

    feat: Add dynamic param in /map #5
    - Add dynamic param

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/Home/HomeView.vue";
+import MapView from "../views/Map/MapView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,19 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: HomeView
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: MapView,
+      children: [
+        {
+          path: ":contentId",
+          name: "reviews",
+          component: MapView
+        }
+      ]
     },
     {
       path: "/about",
@@ -15,7 +28,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/About/AboutView.vue"),
+      component: () => import("../views/About/AboutView.vue")
     },
     {
       path: "/board",
@@ -24,15 +37,15 @@ const router = createRouter({
       children: [
         {
           path: "detail",
-          component: "",
+          component: ""
         },
         {
           path: "detail",
-          component: "",
-        },
-      ],
-    },
-  ],
+          component: ""
+        }
+      ]
+    }
+  ]
 });
 
 export default router;
