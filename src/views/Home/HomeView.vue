ccd3103513102488dcbdabb11be187651001fe2d
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-13 16:26:05 +0900

    chore: MapView #7
    - Modify home view and Create Map view

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,18 +1,7 @@
+<script setup></script>
+
 <template>
-  <main>
-    <div id="map">지도입니다.</div>
-    <TheSide :position-left="0"></TheSide>
-    <!-- <TheSide :position-left="340"></TheSide> -->
-  </main>
+  <div></div>
 </template>
 
-<script setup>
-import { reactive } from "vue";
-import TheSide from "../../components/TheSideBar/TheSideBar.vue";
-
-const sideBarViewStates = reactive({ cardsSideBarView: false, reviewsSideBarView: false });
-</script>
-
-<style lang="scss">
-@import "./HomeView.scss";
-</style>
+<style scoped></style>
---
+++ src/views/Map/MapView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <TheMap :spots="DUMMY_ITEMS"></TheMap>
+    <TheSide :position-left="0"> </TheSide>
+  </main>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import TheSide from "../../components/TheSideBar/TheSideBar.vue";
+import TheMap from "../../components/TheMap/TheMap.vue";
+import { DUMMY_ITEMS } from "../../dummy.js";
+
+const sideBarViewStates = reactive({ cards: true, reviews: true });
+
+const handlers = {
+  card() {
+    sideBarViewStates.cards = !sideBarViewStates.cards;
+  },
+  review() {
+    sideBarViewStates.reviews = !sideBarViewStates.reviews;
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./MapView.scss";
+</style>
