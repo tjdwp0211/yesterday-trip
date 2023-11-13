c1eca98cfecb76f1d3f6d863a2c1329d57bea0e0
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-13 16:23:46 +0900

    chore: Change Dir #5
    - TheReviewCard Comp

--- src/components/TheSideBar/Subs/TheReviewCard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <li class="cards-wrapper"><slot></slot></li>
-</template>
-
-<script setup></script>
-
-<style lang="scss">
-@import "./TheReviewCard.scss";
-</style>
---
+++ src/components/TheSideBar/Subs/TheReviewCard/TheReviewCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterLink :to="`/map/${contentId}`">
+    <li class="cards-wrapper">
+      <slot></slot>
+    </li>
+  </RouterLink>
+</template>
+
+<script setup>
+import { ref, toRefs } from "vue";
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  reviewSideBarHandler: { type: Function, required: true },
+  contentId: { type: Number, required: true }
+});
+const { reviewSideBarHandler } = toRefs(props);
+</script>
+
+<style lang="scss">
+@import "./TheReviewCard.scss";
+</style>
