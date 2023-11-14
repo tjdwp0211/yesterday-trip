6b1ff90a7f90117b78d22c3c512e2972224d14cb
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-15 00:45:39 +0900

    style: SearchInput #5
    - Wrapping Transition
    - Add SearchButton img

--- src/components/TheSearchInput/TheSearchInput.vue
+++ src/components/TheSearchInput/TheSearchInput.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="search-input-wrapper">
-    <Input id="search" type="text" placeholder="검색창" :value="value" @handle-change="handler" />
-  </div>
+  <Transition name="bounce">
+    <div id="search-input-wrapper" v-if="visible">
+      <BaseInput id="search" type="text" placeholder="검색창" :value="value" @handle-change="handler" />
+      <BaseButton
+        type="submit"
+        width="36px"
+        height="36px"
+        color="white"
+        :backgroundColor="MAIN_BLUE"
+        :eventHandler="closeRevieSideBar"
+        :sortCenter="true"
+      >
+        <img src="../../assets/imgs/search.svg" width="12" height="12" />
+      </BaseButton>
+    </div>
+  </Transition>
 </template>
 <script setup>
-import { defineProps, toRefs } from "vue";
-import Input from "../BaseInput/BaseInput.vue";
+import { defineProps, toRefs, Transition } from "vue";
+import { PALETTE } from "../../palette";
+import BaseInput from "../BaseInput/BaseInput.vue";
+import BaseButton from "../BaseButton/BaseButton.vue";
 
 const props = defineProps({
+  visible: { type: Boolean, required: true },
   value: { type: String, required: true },
   handler: { type: Function, required: true }
 });
-
-const { value, handler } = toRefs(props);
+const { value, handler, visible } = toRefs(props);
+const { MAIN_BLUE } = PALETTE;
 </script>
 <style lang="scss">
 @import "./TheSearchInput.scss";
