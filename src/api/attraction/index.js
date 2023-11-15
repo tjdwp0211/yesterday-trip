bac9a90858e343d55f42460f6328135febe7560c
김성제 <rlatjdwp0211@naver.com>
2023-11-15 17:50:46 +0900

    feat: Create Store #12
    - Create Attraction Store
    - Apply useAttractionStore

--- src/api/attraction/index.js
+++ src/api/attraction/index.js
@@ -1,5 +1,15 @@
 import { instance } from "..";
 
-export async function requsetAttraction({ sido, gugun, contentType }) {
-  return await instance.get(`/api/attraction/search?sido=${sido}&gugun=${gugun}&contentType=${contentType}`);
+export async function requsetAttractionByCodes({ sido, gugun, contentType }) {
+  return await instance.get(`/attraction/search?sido=${sido}&gugun=${gugun}&contentType=${contentType}`);
 }
+
+export async function requsetAttractionByKeyword({ keyword }) {
+  return await instance.get(`/attraction/${keyword}`);
+}
+
+// 시도, 군구, 컨텐츠
+// /attraction/search?sido= & gugun= & contentType=
+
+// 키워드
+// /attraction/:keyword
