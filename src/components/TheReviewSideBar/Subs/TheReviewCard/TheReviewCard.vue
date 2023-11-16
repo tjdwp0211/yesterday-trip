666bc07bf905a24ca3efddc4aa8b2b6e7fe3245b
Sung Je Kim <rlatjdwp0211@naver.com>
2023-11-16 09:07:14 +0900

    style: TheReviewCard #5
    - Style work backup

--- src/components/TheReviewSideBar/Subs/TheReviewCard/TheReviewCard.vue
+++ src/components/TheReviewSideBar/Subs/TheReviewCard/TheReviewCard.vue
@@ -1,8 +1,31 @@
 <template>
-  <article class="review-card-wrapper">리뷰 카드</article>
+  <article class="review-card-wrapper">
+    <div class="review-card-container">
+      <div class="texts-wrapper">
+        <p>
+          <span class="review-card-user-nickname">USER NICKNAME</span>
+          <span class="review-card-user-emotion">EMOTION</span>
+          <!-- EMOTION COLOR, BORDER COLOR 동적으로 주기 main blue or main red -->
+        </p>
+        <p class="review-card-create-time">CREATED TIME</p>
+        <p class="review-card-user-content">컨텐츠 들어갈 부분</p>
+      </div>
+
+      <div class="review-card-imgs-wrapper">
+        <img src="../../../../assets/imgs/bars.svg" />
+        <img src="../../../../assets/imgs/bars.svg" />
+        <img src="../../../../assets/imgs/bars.svg" />
+      </div>
+    </div>
+  </article>
 </template>
 
-<script setup></script>
+<script setup>
+// review id
+// string content
+// string emotion
+// image[] images
+</script>
 
 <style lang="scss">
 @import "./TheReviewCard.scss";
